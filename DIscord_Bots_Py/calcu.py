f9b28b62a5ab71a9a22e7efff6e4f1cfd6939cc6
Brain <1>
2020-12-26 17:58:56 -0500

    discord bot

--- DIscord_Bots_Py/calcu.py
+++ DIscord_Bots_Py/calcu.py
@@ -1,3 +1,52 @@
+import sys, discord, os, base64
+sys.path.insert(0,"")  # my path top secret ^-^
+import SciModule_Py
+from github import Github
+from pprint import pprint
+
+
+
+#constant
+client = discord.Client()
+username = "MuscleBrain"
+g = Github()
+
+
+
+pk = SciModule_Py.Physics_Modules.kinemic() 
+
+
+
+def fileExist(Module):
+    Module += "_Modules.py"
+    path = f"" # my path top secret ^-^
+    return os.path.isfile(path) 
+
+
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$'):         # if $
+        user_input = message.content.split()
+        subject = user_input[1]
+        if fileExist(subject) == True:    # ex: Math or Physics
+            q_num = username[2]                 # ex: func1 or func2
+            q_kno = user_input[3]             # ex: [1,2,3,4]
+            answer = exec(f"{subject}._{q_num}_({q_kno})")
+            await message.channel.send(answer)      # result 
+client.run('') # robot key  top secret ^-^
+
+
+
+
 
 
 
--- SciModule_Py/Physics_Modules.py
+++ SciModule_Py/Physics_Modules.py
@@ -5,20 +5,7 @@ All rights reserved.
 Author : Eric
 '''
 
-'''
-\n# <Help>
-\n# In : 
-\n# Out : 
-'''
-'''
-\n# About : 
-\n# Step : 
-\n# Step : 
-\n# Step : 
-\n# Return : 
-'''
-
-def Kinemic_getDistance(v, angle):
+def _1_(q_kno):
     '''
     \n# <Help>
     \n# In : v (velocity) + angle
@@ -30,14 +17,17 @@ def Kinemic_getDistance(v, angle):
     \n# Step : d = ( V1 * t ) - ( 0.5 * a * t**2 ) --> t 
     \n# Return : distance
     '''
-    import math
-    vx, vy = v * math.cos(math.radians(60)), v * math.sin(math.radians(60))
-    t = vy / 4.9
-    d = vx * t
-    return d
+    v, angle = q_kno[0:2]
+    def getDistance(v,angle):
+        import math
+        vx, vy = v * math.cos(math.radians(angle)), v * math.sin(math.radians(angle))
+        t = vy / 4.9
+        d = vx * t
+        return d
+    return getDistance(v,angle)
 
 
-def Kinemic_getDistance_bruteForce(v_min, v_max, angle_min, angle_max, distance):
+def _2_(q_kno):
     '''
     \n# <Help>
     \n# In : v_min , v_max (range)
@@ -49,131 +39,139 @@ def Kinemic_getDistance_bruteForce(v_min, v_max, angle_min, angle_max, distance)
     \n# Step : d = (V1 * t) - (0.5 * a * t**2) --> t 
     \n# Return : V, angle { if int(Vx * t) == distance }
     '''
-    import math
-    for v in range(v_min,v_max):
-        for angle in range(angle_min, angle_max):
-            vx, vy = v * math.cos(math.radians(60)), v * math.sin(math.radians(60))
-            t = vy / 4.9
-            if int(vx * t) == distance:
-                return v , angle    
+    v_min, v_max, angle_min, angle_max, distance = q_kno[0,5]
+    def getDistance_bruteForce(v_min, v_max, angle_min, angle_max, distance):
+        import math
+        for v in range(v_min,v_max):
+            for angle in range(angle_min, angle_max):
+                vx, vy = v * math.cos(math.radians(angle)), v * math.sin(math.radians(angle))
+                t = vy / 4.9
+                if int(vx * t) == distance:
+                    return v , angle    
+    return getDistance_bruteForce(v_min, v_max, angle_min, angle_max, distance)
 
 
-def Kinemic_5Equations(v1, v2, a, t, d):
-    '''
-    \n>> <Help>
-    \n>> In : any three of v1, v2, a, t, d
-    \n>> In : type None for unknown
-    \n>> Return : v1 , v2 , a , d , t
-    '''
-    '''
-    \n# About : five kinemic equations
-    \n# Step : if elem is None --> run value in calculate() 
-    \n# Step : else --> it's good
-    \n# Step : calculate() , four ways to get answers for each value , 
-    \n# Step : keep testing , if it fails , try another solution
-    \n# Return : five values
-    \n#
-    \n# Equation1 : v2 = v1 + (a * t)
-    \n# Equation2 : d = (v1 + v2) / 2  * t
-    \n# Equation3 : d = (v1 * t) + (0.5 * a * t**2)
-    \n# Equation4 : d = (v2 * t) - (0.5 * a * t**2)
-    \n# Equation5 : v2**2 = v1**2 + (2 * a * d)  
-    '''
-    def calculate(elem):
-        try:################################## First term
-            if elem == "v1":
-                elem = v2 - a * t
-                return elem
-            elif elem == "v2":
-                elem = v1 + a * t
-                return elem
-            elif elem == "a":
-                elem = (v2 - v1) / t
-                return elem
-            elif elem == "t":
-                elem = (v2 - v1) / a
-                return elem
-            elif elem == "d":
-                elem = (v1 + v2) * 2 / t
-                return elem
 
-
-        except:
-            try:#################################### second term
+def _3_(q_kno):
+    v1, v2, a, t, d = q_kno[0:5]
+    def fiveEquations(v1, v2, a, t, d):
+        '''
+        \n>> <Help>
+        \n>> In : any three of v1, v2, a, t, d
+        \n>> In : type None for unknown
+        \n>> Return : v1 , v2 , a , d , t
+        '''
+        '''
+        \n# About : five kinemic equations
+        \n# Step : if elem is None --> run value in calculate() 
+        \n# Step : else --> it's good
+        \n# Step : calculate() , four ways to get answers for each value , 
+        \n# Step : keep testing , if it fails , try another solution
+        \n# Return : five values
+        \n#
+        \n# Equation1 : v2 = v1 + (a * t)
+        \n# Equation2 : d = (v1 + v2) / 2  * t
+        \n# Equation3 : d = (v1 * t) + (0.5 * a * t**2)
+        \n# Equation4 : d = (v2 * t) - (0.5 * a * t**2)
+        \n# Equation5 : v2**2 = v1**2 + (2 * a * d)  
+        '''
+        def calculate(elem):
+            try:################################## First term
                 if elem == "v1":
-                    elem = 2*d/t-v2
+                    elem = v2 - a * t
                     return elem
                 elif elem == "v2":
-                    elem = 2*d/t-v1
+                    elem = v1 + a * t
                     return elem
                 elif elem == "a":
-                    elem = (d - v1*t)/(0.5*t**2)
+                    elem = (v2 - v1) / t
                     return elem
                 elif elem == "t":
-                    elem = (2 * d) / (v1 + v2)
+                    elem = (v2 - v1) / a
                     return elem
                 elif elem == "d":
-                    elem = v1 * t + 0.5 * a * t ** 2
+                    elem = (v1 + v2) * 2 / t
                     return elem
 
 
             except:
-                try:###################################### third term
+                try:#################################### second term
                     if elem == "v1":
-                        elem = d/t - 0.5*a*t
+                        elem = 2*d/t-v2
                         return elem
                     elif elem == "v2":
-                        elem = d/t + 0.5*a*t
+                        elem = 2*d/t-v1
                         return elem
                     elif elem == "a":
-                        elem = (v2 * t - d) / (0.5 * t ** 2)
+                        elem = (d - v1*t)/(0.5*t**2)
                         return elem
                     elif elem == "t":
-                        elem = (- v1 + (v1 ** 2 + 2 * a * d) ** 0.5) / a
+                        elem = (2 * d) / (v1 + v2)
                         return elem
                     elif elem == "d":
-                        elem = v2 * t - 0.5 * a * t ** 2
+                        elem = v1 * t + 0.5 * a * t ** 2
                         return elem
 
 
                 except:
-                    try:######################################## fourth term
+                    try:###################################### third term
                         if elem == "v1":
-                            elem = (v2**2 - 2*a*d) **0.5
+                            elem = d/t - 0.5*a*t
                             return elem
                         elif elem == "v2":
-                            elem = (v1**2 + 2*a*d) **0.5
+                            elem = d/t + 0.5*a*t
                             return elem
                         elif elem == "a":
-                            elem = (v2 ** 2 - v1 ** 2) / (2 * d)
+                            elem = (v2 * t - d) / (0.5 * t ** 2)
                             return elem
                         elif elem == "t":
-                            elem = (- v1 - (v1**2 + 2*a*d)**0.5) / a
+                            elem = (- v1 + (v1 ** 2 + 2 * a * d) ** 0.5) / a
                             return elem
                         elif elem == "d":
-                            elem = (v2 ** 2 - v1 ** 2)/(2 * a)
+                            elem = v2 * t - 0.5 * a * t ** 2
                             return elem
+
+
                     except:
-                        try:
-                            if elem == "t":
-                                elem = (- v2 + (v1**2 - 2*a*d)**0.5) / a
+                        try:######################################## fourth term
+                            if elem == "v1":
+                                elem = (v2**2 - 2*a*d) **0.5
+                                return elem
+                            elif elem == "v2":
+                                elem = (v1**2 + 2*a*d) **0.5
+                                return elem
+                            elif elem == "a":
+                                elem = (v2 ** 2 - v1 ** 2) / (2 * d)
+                                return elem
+                            elif elem == "t":
+                                elem = (- v1 - (v1**2 + 2*a*d)**0.5) / a
+                                return elem
+                            elif elem == "d":
+                                elem = (v2 ** 2 - v1 ** 2)/(2 * a)
                                 return elem
                         except:
                             try:
                                 if elem == "t":
-                                    elem = (- v2 - (v1**2 - 2*a*d)**0.5) / a
+                                    elem = (- v2 + (v1**2 - 2*a*d)**0.5) / a
                                     return elem
                             except:
-                                print("O M G !")
-                                
-    
-    list1 = [v1, v2, a, t, d]
-    list2 = ["v1", "v2", "a", "t", "d"]
-    list3 = []
-    for elem in range(len(list1)):
-        if list1[elem] == None:
-            list3.append(calculate(list2[elem]))    
-        else:
-            list3.append(list1[elem])
-    return f"v1 = {list3[0]}, v2 = {list3[1]}, a = {list3[2]}, t = {list3[3]}, d = {list3[4]}"
+                                try:
+                                    if elem == "t":
+                                        elem = (- v2 - (v1**2 - 2*a*d)**0.5) / a
+                                        return elem
+                                except:
+                                    print("O M G !")
+                                    
+        
+        list1 = [v1, v2, a, t, d]
+        list2 = ["v1", "v2", "a", "t", "d"]
+        list3 = []
+        for elem in range(len(list1)):
+            if list1[elem] == None:
+                list3.append(calculate(list2[elem]))    
+            else:
+                list3.append(list1[elem])
+        return f"v1 = {list3[0]}, v2 = {list3[1]}, a = {list3[2]}, t = {list3[3]}, d = {list3[4]}"
+    return fiveEquations(v1, v2, a, t, d)
+
 
